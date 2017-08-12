47a70fa4f6a54c2b3c891263566047544d55481d
Christopher Maree <christopher.maree@gmail.com>
2017-08-12 17:38:52 +0200

    changed background colour a bit

--- Landing page ideas/Landing Page2/js/index.js
+++ Landing page ideas/Landing Page2/js/index.js
@@ -293,26 +293,6 @@ Particle.prototype = (function(o) {
         this._latest.set(this);
         this.add(this._speed);
     }
-
-    // render: function(ctx) {
-    //     if (this._speed.length() > 12) this._speed.normalize().scale(12);
-
-    //     this._latest.set(this);
-    //     this.add(this._speed);
-
-    //     ctx.save();
-    //     ctx.fillStyle = ctx.strokeStyle = '#fff';
-    //     ctx.lineCap = ctx.lineJoin = 'round';
-    //     ctx.lineWidth = this.radius * 2;
-    //     ctx.beginPath();
-    //     ctx.moveTo(this.x, this.y);
-    //     ctx.lineTo(this._latest.x, this._latest.y);
-    //     ctx.stroke();
-    //     ctx.beginPath();
-    //     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    //     ctx.fill();
-    //     ctx.restore();
-    // }
 });
 
 
@@ -323,7 +303,7 @@ Particle.prototype = (function(o) {
 
     // Configs
 
-    var BACKGROUND_COLOR      = 'rgba(11, 51, 56, 1)',
+    var BACKGROUND_COLOR      = 'rgba(51, 204, 255,1)',
         PARTICLE_RADIUS       = 1,
         G_POINT_RADIUS        = 10,
         G_POINT_RADIUS_LIMITS = 65;
@@ -452,22 +432,6 @@ Particle.prototype = (function(o) {
     canvas.addEventListener('dblclick', doubleClick, false);
 
 
-    // // GUI
-
-    // gui = new dat.GUI();
-    // gui.add(control, 'particleNum', 0, 500).step(1).name('Particle Num').onChange(function() {
-    //     var n = (control.particleNum | 0) - particles.length;
-    //     if (n > 0)
-    //         addParticle(n);
-    //     else if (n < 0)
-    //         removeParticle(-n);
-    // });
-    // gui.add(GravityPoint, 'interferenceToPoint').name('Interference Between Point');
-    // gui.close();
-
-
-    // Start Update
-
     var loop = function() {
         var i, len, g, p;
 
@@ -495,10 +459,7 @@ Particle.prototype = (function(o) {
         bufferCtx.fillRect(0, 0, screenWidth, screenHeight);
         bufferCtx.restore();
 
-        // パーティクルをバッファに描画
-        // for (i = 0, len = particles.length; i < len; i++) {
-        //     particles[i].render(bufferCtx);
-        // }
+       
         len = particles.length;
         bufferCtx.save();
         bufferCtx.fillStyle = bufferCtx.strokeStyle = '#fff';
@@ -521,7 +482,6 @@ Particle.prototype = (function(o) {
         bufferCtx.fill();
         bufferCtx.restore();
 
-        // バッファをキャンバスに描画
         context.drawImage(bufferCvs, 0, 0);
 
         requestAnimationFrame(loop);
