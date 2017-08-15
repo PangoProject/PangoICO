213359e048333128c9be856f28b3772b1b19b904
Christopher Maree <christopher.maree@gmail.com>
2017-08-16 01:29:15 +0200

    Added navbar colour changing based on current section of page

--- MainICOPage/assets/js/scroll-to-id.js
+++ MainICOPage/assets/js/scroll-to-id.js
@@ -1,35 +1,40 @@
-$('a[href*="#"]')
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
-        if (
-            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-            &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                event.preventDefault();
-                $('html, body').animate({
-                    scrollTop: target.offset().top-100
-                }, 1000, function() {
-                    var $target = $(target);
-                    $target.focus();
-                    if ($target.is(":focus")) {
-                        return false;
-                    } else {
-                        $target.attr('tabindex','-1');
-                        $target.focus();
-                    };
-                });
-            }
-        }
+//Scroll to ID and change active selector on scroll
+var lastId,
+    topMenu = $("#top-menu"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
     });
 
-$("a[href='#top']").click(function() {
-    $("html, body").animate({ scrollTop: 0 }, 1000);
-    return false;
+menuItems.click(function(e){
+        var href = $(this).attr("href"),
+        offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+    $('html, body').stop().animate({
+        scrollTop: offsetTop
+    }, 1000);
+    e.preventDefault();
+});
+
+
+$(window).scroll(function(){
+    var fromTop = $(this).scrollTop()+topMenuHeight;
+    var cur = scrollItems.map(function(){
+        if ($(this).offset().top - 100 < fromTop)
+            return this;
+    });
+    cur = cur[cur.length-1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+        lastId = id;
+        menuItems
+            .parent().removeClass("active animated fadeIn")
+            .end().filter("[href='#"+id+"']").parent().addClass("active animated fadeIn");
+    }
 });
 
 // Check distance to top and display back-to-top.
@@ -39,4 +44,9 @@ $( window ).scroll( function() {
     } else {
         $( '.back-to-top' ).removeClass( 'show-back-to-top' );
     }
+});
+
+$("a[href='#top']").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+    return false;
 });
